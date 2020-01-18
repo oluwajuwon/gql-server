53e0700f8a2f1267b247167f85352780b259b886
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2020-01-18 21:57:21 +0100

    add sessions for logged in user

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import { createConnection } from 'typeorm';
 import { ApolloServer } from 'apollo-server-express';
 import * as express from 'express';
+import * as session from 'express-session';
 
 import { typeDefs } from './typeDefs';
 import { resolvers } from './resolvers';
@@ -11,11 +12,18 @@ const startServer = async () => {
         // These will be defined for both new or existing servers
         typeDefs,
         resolvers,
+        context: ({ req }: any) => ({ req })
     });
 
     await createConnection();
 
     const app = express();
+
+    app.use(session ({
+        secret: 'boooky',
+        resave: false,
+        saveUninitialized: false,
+    }))
 
     server.applyMiddleware({ app })
     // Additional middleware can be mounted at this point to run before Apollo.
